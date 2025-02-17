bfab8b92dc7fd39e6a1f368ab6247fbb9719606b
Isaac Norris <isaac.a.norris@gmail.com>
2025-02-17 17:05:50 +1100

    system commin

--- source/mini_pc/src/main.cpp
+++ source/mini_pc/src/main.cpp
@@ -1,18 +1,12 @@
 #include <Arduino.h>
+#include "System.h"
 
-// put function declarations here:
-int myFunction(int, int);
+tSystem system;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  system.SystemStartup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  system.SystemLoop();
 }
