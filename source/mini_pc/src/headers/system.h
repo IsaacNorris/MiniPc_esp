f94fa11586b7087e89292b7b9ba9e03a722f4c7b
Isaac Norris <isaac.a.norris@gmail.com>
2025-07-10 03:41:47 +1000

    removed todos.

--- source/mini_pc/src/headers/system.h
+++ source/mini_pc/src/headers/system.h
@@ -38,22 +38,3 @@ private:
     tStateManager states_;
     tSysClock clock_;
 };
-
-// TODO: add in the millis functions so that i can properly controll the time.
-
-// TODO: add the menu options similar to the old watch interface
-
-// TODO: add a storage manager
-
-// TODO: add a stopwatch process
-// TODO: add a timer process
-
-// TODO: add a adxl device
-// TODO: add a step tracker process
-
-// TODO: add a led graphic that is connected to the clock/timer/stopwatch processes
-
-// TODO: figure out how to run multithread processes
-
-// TODO: finish comms/network/bluetooth managers and link them to do functional things
-//  - talk to ben about what the bluetooth/wifi would do.
