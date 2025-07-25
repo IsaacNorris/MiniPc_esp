d718bda2cfbf2aeb6bf31a67afa550fd321f6141
Isaac Norris <isaac.a.norris@gmail.com>
2025-07-25 18:01:01 +1000

    moved over to the ESP32 C3

--- source/mini_pc/src/headers/constant.h
+++ source/mini_pc/src/headers/constant.h
@@ -2,6 +2,10 @@
 
 // Change the pins to what they should be for the buttons here
 
-#define BUTTON_UP_PIN 25 //25 on MiniPC
-#define BUTTON_DOWN_PIN 33 //33 on MiniPC
-#define BUTTON_ENTER_PIN 35 //35 on MiniPC
+// #define BUTTON_UP_PIN 25 //25 on MiniPC
+// #define BUTTON_DOWN_PIN 33 //33 on MiniPC
+// #define BUTTON_ENTER_PIN 35 //35 on MiniPC
+
+#define BUTTON_UP_PIN 2 //25 on MiniPC
+#define BUTTON_DOWN_PIN 3 //33 on MiniPC
+#define BUTTON_ENTER_PIN 1 //35 on MiniPC
