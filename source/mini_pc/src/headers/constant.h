c832e561920424c5fa13231f9c43e0b0da856b7d
Isaac Norris <isaac.a.norris@gmail.com>
2025-07-29 13:55:37 +1000

    fixed issue with C3 board settings.

--- source/mini_pc/src/headers/constant.h
+++ source/mini_pc/src/headers/constant.h
@@ -6,6 +6,6 @@
 // #define BUTTON_DOWN_PIN 33 //33 on MiniPC
 // #define BUTTON_ENTER_PIN 35 //35 on MiniPC
 
-#define BUTTON_UP_PIN 2 //25 on MiniPC
-#define BUTTON_DOWN_PIN 3 //33 on MiniPC
-#define BUTTON_ENTER_PIN 1 //35 on MiniPC
+#define BUTTON_UP_PIN D2 //25 on MiniPC
+#define BUTTON_DOWN_PIN D3 //33 on MiniPC
+#define BUTTON_ENTER_PIN D1 //35 on MiniPC
